fed2d41dbf37fc6e2932c4e497cb637e6a5c70b1
HanzhaP <pganza94@gmail.com>
2025-05-06 12:50:18 +0300

    Виконані завдання

---
+++ lesson_06/homework_06_1.py
@@ -0,0 +1,14 @@
+# Порахувати кількість унікальних символів в строці. Якщо їх більше 10 - вивести в консоль True,
+# інакше - False. Строку отримати за допомогою функції input()
+#
+
+received_text = input('Enter any text:\n')
+length_unique_received_symbols = len(set(received_text))
+print(len(set(received_text)))
+if 10 < length_unique_received_symbols:
+    print(True)
+if 10 > length_unique_received_symbols:
+    print(False)
+
+
+
