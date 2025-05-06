bc7f99bc50c645c395452480fe03ea8df75b86dc
HanzhaP <pganza94@gmail.com>
2025-05-06 16:49:32 +0300

    Виконані завдання

--- lesson_06/homework_06_1.py
+++ lesson_06/homework_06_1.py
@@ -10,3 +10,17 @@ if 10 < length_unique_received_symbols:
 if 10 > length_unique_received_symbols:
     print(False)
 
+# Напишіть цикл, який буде вимагати від користувача ввести слово, в якому є літера "h"
+# (враховуються як великі так і маленькі). Цикл не повинен завершитися, якщо користувач ввів слово без букви "h".
+
+received_word = input('Enter any word with letter "h":\n')
+counter = 1
+
+for k in received_word:
+    if k == 'h' or k == 'H':
+        print('In your word present "h" or "H" letter')
+        break
+    if k != 'h' or k != 'H':
+        counter += 1
+    if len(received_word) - 1 == counter:
+        received_word = input('Write another word, because in previous word absent letter "h" or "H"\n')
