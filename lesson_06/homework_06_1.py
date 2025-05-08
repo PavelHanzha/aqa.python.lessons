d518dfc591d6e4d803c0332edb9d28637997063b
HanzhaP <pganza94@gmail.com>
2025-05-08 22:01:17 +0300

    Виконані завдання

--- lesson_06/homework_06_1.py
+++ lesson_06/homework_06_1.py
@@ -56,6 +56,6 @@ list_of_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 1, 61, 43, 
                   654, 34]
 sum_numbers = 0
 for k in list_of_number:
-    if k%2 == 0:
+    if k % 2 == 0:
         sum_numbers = sum_numbers + k
 print(sum_numbers)
