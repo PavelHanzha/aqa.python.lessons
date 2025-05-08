e2a823fcfcca89cfd0605cbc2ba2b2a24599aa51
HanzhaP <pganza94@gmail.com>
2025-05-08 21:51:53 +0300

    Виконані завдання

--- lesson_06/homework_06_1.py
+++ lesson_06/homework_06_1.py
@@ -2,39 +2,51 @@
 # інакше - False. Строку отримати за допомогою функції input()
 #
 
-received_text = input('Enter any text:\n')
-length_unique_received_symbols = len(set(received_text))
-print(len(set(received_text)))
-if 10 < length_unique_received_symbols:
-    print(True)
-if 10 > length_unique_received_symbols:
-    print(False)
-
-# Напишіть цикл, який буде вимагати від користувача ввести слово, в якому є літера "h"
-# (враховуються як великі так і маленькі). Цикл не повинен завершитися, якщо користувач ввів слово без букви "h".
-
-# 1st attempt via for
-# received_word = input('Enter any word with letter "h":\n')
-# counter = 1
+# received_text = input('Enter any text:\n')
+# length_unique_received_symbols = len(set(received_text))
+# print(len(set(received_text)))
+# if 10 < length_unique_received_symbols:
+#     print(True)
+# if 10 > length_unique_received_symbols:
+#     print(False)
 #
-# for k in received_word:
-#     if k == 'h' or k == 'H':
-#         print('In your word present "h" or "H" letter')
+# # Напишіть цикл, який буде вимагати від користувача ввести слово, в якому є літера "h"
+# # (враховуються як великі так і маленькі). Цикл не повинен завершитися, якщо користувач ввів слово без букви "h".
+#
+# # 1st attempt via for
+# # received_word = input('Enter any word with letter "h":\n')
+# # counter = 1
+# #
+# # for k in received_word:
+# #     if k == 'h' or k == 'H':
+# #         print('In your word present "h" or "H" letter')
+# #         break
+# #     if k != 'h' or k != 'H':
+# #         counter += 1
+# #     if len(received_word) - 1 == counter:
+# #         received_word = input('Write another word, because in previous word absent letter "h" or "H"\n')
+#
+# # 2nd attempt via while
+#
+# while True:
+#     receive_word_2 = input('Enter any word with letter "h":\n')
+#     if 'h' in receive_word_2:
+#         print('Letter "h" present')
 #         break
-#     if k != 'h' or k != 'H':
-#         counter += 1
-#     if len(received_word) - 1 == counter:
-#         received_word = input('Write another word, because in previous word absent letter "h" or "H"\n')
+#     if 'H' in receive_word_2:
+#         print('Letter "H" present')
+#         break
+#     else:
+#         print('absent letter "h" or "H"')
 
-# 2nd attempt via while
+# Є list з даними lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum']. Напишіть код, який
+# свормує новий list (наприклад lst2), який містить лише змінні типу стрінг, які присутні в lst1.
+# Данні в лісті можуть бути будь якими
 
-while True:
-    receive_word_2 = input('Enter any word with letter "h":\n')
-    if 'h' in receive_word_2:
-        print('Letter "h" present')
-        break
-    if 'H' in receive_word_2:
-        print('Letter "H" present')
-        break
-    else:
-        print('absent letter "h" or "H"')
+lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum']
+lst2 = []
+for k in lst1:
+    if type(k) == str:
+        lst2.append(k)
+print(lst2)
+
